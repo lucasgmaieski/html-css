70b4163bc4bade465b63df69705ec55e5d77ab9e
Lucas Maieski <lucasgrigol@gmail.com>
2022-10-05 22:36:53 -0300

    Adicionando estudos de JS e menu hamburguer

---
+++ Estudos-JavaScript/01-primeiro-programa/js/index.js
@@ -0,0 +1,22 @@
+// alert("opaaaaa tamo falando aqui do javascript ")  
+// console.log("ola mundo")
+
+var nomePessoa = "Lucas"
+
+// const depois de declarado não pode ser mudado 
+const emailPessoa = "lucas@gmail.com"
+console.log(emailPessoa)
+
+let telefonePessoa = "46 99133 3202"
+telefonePessoa = "46991333202"
+// let permite mudar o valor da variavel
+console.log(telefonePessoa)
+
+// escopo global - 
+// escopo de função- 
+// escopo de bloco(dentro de chaves if else) -> const e let não funcionam aqui  
+
+
+let retornoDoUsuario = confirm("Deseja continuar?")
+console.log(retornoDoUsuario)
+
