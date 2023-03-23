6d34772285bec07f7679dad10703bb84d4eed7c8
Lucas Maieski <lucasgrigol@gmail.com>
2023-03-22 23:15:22 -0300

    criando projeto react com vite

---
+++ React/react-vite/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  // return React.createElement('div', null, 'Hello Word!');
+  return React.createElement('a', { href: 'https://b7web.com.br'}, 'Clique Aqui!');
+}
+
+
+// function App() {
+//   return (
+//     <div> Hello Word!</div>
+//   );
+// }
+
+export default App;
