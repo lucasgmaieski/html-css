42f52997c549066143277b3456b191ae0a260393
Lucas Maieski <lucasgrigol@gmail.com>
2023-03-23 22:45:28 -0300

    criando primeiro componente

--- React/react-vite/src/App.tsx
+++ React/react-vite/src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Header } from './components/Header';
 
-const App = () => {
-  // return React.createElement('div', null, 'Hello Word!');
-  return React.createElement('a', { href: 'https://b7web.com.br'}, 'Clique Aqui!');
-}
-
-
+// const App = () => {
+//   // return React.createElement('div', null, 'Hello Word!');
+//   return React.createElement('a', { href: 'https://b7web.com.br'}, 'Clique Aqui!');
+// }
 // function App() {
 //   return (
 //     <div> Hello Word!</div>
 //   );
 // }
+
+// expressões no jsx
+const App = () => {
+
+  return (
+    <div>
+      <Header title="Este é um título de exemplo passado por props"/>
+      <Header title="Este é um título secundario de exemplo passado por props"/>
+      Hello Word!
+    </div>
+  );
+}
 
 export default App;
---
+++ React/react-vite/src/components/Header.tsx
@@ -0,0 +1,15 @@
+type Props = {
+    title: string;
+    // title?: string
+    // deixa essa prop opcional e assim posso usar o componente sem passar props
+}
+
+// forma alternativa de passar a props  ({ title }: Props)   ai pode usar só title no componente ao invés de props.title
+export const Header = (props: Props) => {
+    return (
+        <header>
+            <h1>{props.title}</h1>
+            <hr />
+        </header>
+    );
+}
