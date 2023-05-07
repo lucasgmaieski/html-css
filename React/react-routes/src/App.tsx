3d54fad5a09776cdc75f6163868ecc1dd5b954de
Lucas Maieski~ <lucasgrigol@gmail.com>
2023-05-06 23:13:57 -0300

    adicionado react router ao projeto

---
+++ React/react-routes/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-orange-600 text-3xl font-bold underline'>
+      ...
+      <br />
+      <br />
+      aeee
+    </div>
+  )
+}
+
+export default App
