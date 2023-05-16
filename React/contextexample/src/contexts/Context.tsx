f1fe154c2e8fd2c12b0487b0f999646a8332f5ee
Lucas Maieski <lucasgrigol@gmail.com>
2023-05-15 22:42:43 -0300

    Criando componente de Contexto

---
+++ React/contextexample/src/contexts/Context.tsx
@@ -0,0 +1,21 @@
+import React, { createContext } from "react";
+
+type ContextType = {
+    name: string;
+    age: number;
+}
+
+const initialState = {
+    name: 'Lucas',
+    age: 26
+}
+
+export const Context = createContext<ContextType>(initialState);
+
+export const ContextProvider = ({ children }: React.PropsWithChildren) => {
+    return (
+        <Context.Provider value={initialState}>
+            {children}
+        </Context.Provider>
+    );
+}
