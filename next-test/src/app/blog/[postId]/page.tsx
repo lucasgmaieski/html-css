a691f336fb428068ba17495a90c3ed501b1eaf8e
Lucas Maieski <lucasgrigol@gmail.com>
2023-10-09 23:28:20 -0300

    gerando parametros para rotas dinamicas

--- next-test/src/app/blog/[postId]/page.tsx
+++ next-test/src/app/blog/[postId]/page.tsx
@@ -3,23 +3,32 @@ import { Metadata } from "next";
 
 type Props = {
     params: {
-        slug: string;
+        postId: string;
     };
-    post: Post;
 }
 
 export async function generateMetadata({params}: Props): Promise<Metadata> {
     return {
-        title: `Post ${params.slug}`,
-        description: `Página do Post ${params.slug}`,
+        title: `Post ${params.postId}`,
+        description: `Página do Post ${params.postId}`,
     }
 }
 
-export default async function BlogPost({params, post}: Props) {
+export default async function BlogPost({params}: Props) {
+    // const {slug, postId } = params;
     return(
         <main>
-            <h1>Título blog: {post.title}</h1>
+            <h1>Título blog: {params.postId}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam iusto in aut quasi corporis, debitis atque est similique doloribus repudiandae, earum deleniti cumque officiis blanditiis voluptate, at laboriosam quas rem.</p>
         </main>
     )
 }
+
+export async function generateStaticParams() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', { next: { revalidate: 3600 } });
+    const posts: Post[] = await res.json();
+    return posts.map((post) => ({
+      postId: post.id.toString(),
+      
+    }))
+  }
